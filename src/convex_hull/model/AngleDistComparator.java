fcaba473c8c2310accdbbdfd2cbdd0a008b138cd
Lukas Fuhrmann <fuhrma09@gw.uni-passau.de>
2016-11-18 19:56:00 +0100

    Version 1.1

---
+++ src/convex_hull/model/AngleDistComparator.java
@@ -0,0 +1,25 @@
+package convex_hull.model;
+
+import java.util.Comparator;
+
+/**
+ *
+ */
+public class AngleDistComparator implements Comparator<Point> {
+
+
+    private Point referenceP;
+
+    public Point getReferenceP() {
+        return referenceP;
+    }
+
+    public void setReferenceP(Point referenceP) {
+        this.referenceP = referenceP;
+    }
+
+    public int compare(Point , Point) {
+
+    }
+
+}
